bdc12a6d545c10ac5cd880256429f9a256a57bdd
cpoirier2022 <89803546+cpoirier2022@users.noreply.github.com>
2021-09-21 14:18:08 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
+import Amplify from 'aws-amplify';
 import DisplayPosts from './components/DisplayPosts'
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
 
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
       <CreatePost />
       <DisplayPosts />
 
 
     </div>
+    </AmplifyAuthenticator>
   );
 }
 
