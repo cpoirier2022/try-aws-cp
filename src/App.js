e4afac3c1abd60290587b7833656e7dbff3c24bc
cpoirier2022 <89803546+cpoirier2022@users.noreply.github.com>
2022-01-27 14:49:41 -0500

    maybe

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
-import Amplify from 'aws-amplify';
 import DisplayPosts from './components/DisplayPosts'
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
 
 function App() {
   return (
-    <AmplifyAuthenticator>
     <div className="App">
       <CreatePost />
       <DisplayPosts />
     </div>
-    </AmplifyAuthenticator>
   );
 }
 
